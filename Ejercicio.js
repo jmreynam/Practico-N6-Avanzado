9c2301345602c6bdf45574189d0fdadde3f4b62e
jmreynam <jmreynam@gmail.com>
2022-08-08 23:02:42 -0300

    fin ej

--- Ejercicio.js
+++ Ejercicio.js
@@ -2,6 +2,17 @@ function esNatural(num) {
     // Devuelve true si el num es Natural
     // Usa Recursividad
 }
+function esNatural(num) {
+    
+    if(num<0) return false;
+    if(num===0) return true;
+    return esNatural(num-1);
+
+}
+console.log(esNatural(5))
+console.log(esNatural(-5))
+console.log(esNatural(0))
+//--------------------------------------------------------------------------
 
 function Colores(colores) {
     // Aquellos colores negros cambiar su palabra a Black
@@ -9,16 +20,36 @@ function Colores(colores) {
 }
 
 var colores = ['Blanco', 'Negros', 'Rojo', 'Amarillo', 'Negros', 'Verde']
+function Colores(colores) {
+    var newArray = [];
+    newArray = colores.map(function(x){return x==="Negros"?"Black": x});
+    return newArray;
+}
+var aFiltrar = Colores(colores)
+console.log(aFiltrar);
 
 function Filtrado(colores) {
     // Del resultado de la funcion Colores() filtra aquellos colores llamados Black
     // ejemplo ---> ['Blanco', 'Rojo', 'Amarillo', 'Verde']
 }
+function Filtrado(colores) {
+    return colores.filter(color => color!=="Black")
+}
+console.log(Filtrado(aFiltrar))
+
+//--------------------------------------------------------------------------
+
+
 
 function Filtrar(personas){
     // Filtrar aquellas personas que sean menores de edad, siendo mayor de edad a partir de los 18 años
     // Mostrar arreglo
 }
+
+function Filtrar(personas){
+    return personas.filter(personas => personas.edad>18)
+}
+
 
 const personas = [
 
@@ -30,6 +61,8 @@ const personas = [
     { nombre: 'Mirtha', edad: 17}
 ];
 
+console.log(Filtrar(personas));
+//--------------------------------------------------------------------------
 
 
 
